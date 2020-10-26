ea82f7078152ad4d6e44aea063d01b7d3700ce47
Bob Lam <boblam.ceo@gmail.com>
2020-10-26 19:01:35 +0800

    Updates

---
+++ precache-manifest.7c964cd86ba5a0e4810d2442cc35457f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a8bc26695ca491509697c3b058612c",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "24cd8723ddd44b5ea2b0",
+    "url": "/COVID19-Tracker/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7d168023ddb84e7266ee",
+    "url": "/COVID19-Tracker/static/css/main.b6454242.chunk.css"
+  },
+  {
+    "revision": "24cd8723ddd44b5ea2b0",
+    "url": "/COVID19-Tracker/static/js/2.4704a473.chunk.js"
+  },
+  {
+    "revision": "cc93f7cbb150aa4dc85e1efd721dcc3d",
+    "url": "/COVID19-Tracker/static/js/2.4704a473.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d168023ddb84e7266ee",
+    "url": "/COVID19-Tracker/static/js/main.f68c31c7.chunk.js"
+  },
+  {
+    "revision": "5b2f52aa2bc7f1ce4280",
+    "url": "/COVID19-Tracker/static/js/runtime-main.1cbbe858.js"
+  },
+  {
+    "revision": "8172e304ac0a2f107ad7645a80fc692c",
+    "url": "/COVID19-Tracker/static/media/logo.8172e304.svg"
+  }
+]);
